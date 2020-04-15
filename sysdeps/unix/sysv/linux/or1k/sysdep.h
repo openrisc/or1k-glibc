b180b6c124bc69efecb3abe41b871076256369da
Stafford Horne <shorne@gmail.com>
2020-04-15 22:04:14 +0900

    or1k: sysdep: dont clobber syscall regs when we have memcpy
    This fixes a issue with posix_spawn which uses rt_procmask sycall.
    The mask is about 0x80 bytes and setting the function arg requires
    a builtin_memcpy.  That memcpy is carried out during argument setting.
    When the arguments are required to be in registers they get clobbered.
    
    It seems a similar fix was made in mips.  The fix is to first store
    the arguments to long registers then to register long registers to
    avoid the register clobbering.
    
    This was pointed out by support/test_capture_process.

--- sysdeps/unix/sysv/linux/or1k/sysdep.h
+++ sysdeps/unix/sysv/linux/or1k/sysdep.h
@@ -106,37 +106,24 @@ extern long int __syscall_error (long int neg_errno);
 
 #undef INTERNAL_SYSCALL
 #define INTERNAL_SYSCALL(name, err, nr, args...) \
-        ({ unsigned long __sys_result;                                         \
-          {                                                                    \
-            register long __sc_ret __asm__ ("r11") = SYS_ify (name);           \
-            LOAD_ARGS_##nr (args)                                              \
-            __asm__ __volatile__ ("l.sys     1"                                \
-                                           : "=r" (__sc_ret) ASM_ARGS_OUT_##nr \
-                                           : "0" (__sc_ret) ASM_ARGS_IN_##nr   \
-                         : ASM_CLOBBERS_##nr                                   \
-                           "r12", "r13", "r15", "r17", "r19",                  \
-                           "r21", "r23", "r25", "r27", "r29",                  \
-                           "r31", "memory");                                   \
-            __asm__ __volatile__ ("l.nop");                                    \
-            __sys_result = __sc_ret;                                           \
-          }                                                                    \
-          (long) __sys_result; })
+	INTERNAL_SYSCALL_NCS (SYS_ify (name), err, nr, args)
 
 /* The _NCS variant allows non-constant syscall numbers.  */
 #undef INTERNAL_SYSCALL_NCS
-#define INTERNAL_SYSCALL_NCS(name, err, nr, args...) \
+#define INTERNAL_SYSCALL_NCS(number, err, nr, args...) \
         ({ unsigned long __sys_result;                                         \
           {                                                                    \
-            register long __sc_ret __asm__ ("r11") = name;                     \
+            long _sc_ret = (long) number;                                      \
             LOAD_ARGS_##nr (args)                                              \
-            __asm__ __volatile__ ("l.sys     1"                                \
-                                           : "=r" (__sc_ret) ASM_ARGS_OUT_##nr \
-                                           : "0" (__sc_ret) ASM_ARGS_IN_##nr   \
-                         : ASM_CLOBBERS_##nr                                   \
-                           "r12", "r13", "r15", "r17", "r19",                  \
-                           "r21", "r23", "r25", "r27", "r29",                  \
-                           "r31", "memory");                                   \
-            __asm__ __volatile__ ("l.nop");                                    \
+            register long __sc_ret __asm__ ("r11") = _sc_ret;                  \
+            __asm__ __volatile__ ("l.sys 1\n\t"                                \
+				  " l.nop\n\t"                                 \
+                                  : "+r" (__sc_ret)                            \
+                                  : ASM_ARGS_##nr                              \
+                                  : ASM_CLOBBERS_##nr                          \
+                                    "r12", "r13", "r15", "r17", "r19",         \
+                                    "r21", "r23", "r25", "r27", "r29",         \
+                                    "r31", "memory");                          \
             __sys_result = __sc_ret;                                           \
           }                                                                    \
           (long) __sys_result; })
@@ -166,50 +153,48 @@ extern long int __syscall_error (long int neg_errno);
 
 #define LOAD_ARGS_0()
 
-#define ASM_ARGS_OUT_0
-#define ASM_ARGS_IN_0
+#define ASM_ARGS_0
 #define ASM_CLOBBERS_0  "r3", ASM_CLOBBERS_1
 
-#define LOAD_ARGS_1(a)        \
-    LOAD_ARGS_0 ()        \
-  register long __a __asm__ ("r3") = (long)(a);
-#define ASM_ARGS_OUT_1  ASM_ARGS_OUT_0, "=r" (__a)
-#define ASM_ARGS_IN_1 ASM_ARGS_IN_0, "1" (__a)
+#define LOAD_ARGS_1(a) \
+  long _a = (long)(a);                                      \
+  register long __a __asm__ ("r3") = _a;
+#define ASM_ARGS_1 "r" (__a)
 #define ASM_CLOBBERS_1  "r4", ASM_CLOBBERS_2
 
-#define LOAD_ARGS_2(a, b)     \
-    LOAD_ARGS_1 (a)       \
-  register long __b __asm__ ("r4") = (long)(b);
-#define ASM_ARGS_OUT_2  ASM_ARGS_OUT_1, "=r" (__b)
-#define ASM_ARGS_IN_2 ASM_ARGS_IN_1, "2" (__b)
+#define LOAD_ARGS_2(a, b) \
+  long _b = (long)(b);                                     \
+  LOAD_ARGS_1 (a)                                          \
+  register long __b __asm__ ("r4") = _b;
+#define ASM_ARGS_2 ASM_ARGS_1, "r" (__b)
 #define ASM_CLOBBERS_2  "r5", ASM_CLOBBERS_3
 
-#define LOAD_ARGS_3(a, b, c)      \
-    LOAD_ARGS_2 (a, b)        \
-  register long __c __asm__ ("r5") = (long)(c);
-#define ASM_ARGS_OUT_3  ASM_ARGS_OUT_2, "=r" (__c)
-#define ASM_ARGS_IN_3 ASM_ARGS_IN_2, "3" (__c)
+#define LOAD_ARGS_3(a, b, c) \
+  long _c = (long)(c);                                     \
+  LOAD_ARGS_2 (a, b)                                       \
+  register long __c __asm__ ("r5") = _c;
+#define ASM_ARGS_3 ASM_ARGS_2, "r" (__c)
 #define ASM_CLOBBERS_3  "r6", ASM_CLOBBERS_4
 
-#define LOAD_ARGS_4(a, b, c, d)     \
-    LOAD_ARGS_3 (a, b, c)       \
-  register long __d __asm__ ("r6") = (long)(d);
-#define ASM_ARGS_OUT_4  ASM_ARGS_OUT_3, "=r" (__d)
-#define ASM_ARGS_IN_4 ASM_ARGS_IN_3, "4" (__d)
+#define LOAD_ARGS_4(a, b, c, d) \
+    LOAD_ARGS_3 (a, b, c)                                  \
+  long _d = (long)(d);                                     \
+  register long __d __asm__ ("r6") = _d;
+#define ASM_ARGS_4 ASM_ARGS_3, "r" (__d)
 #define ASM_CLOBBERS_4  "r7", ASM_CLOBBERS_5
 
-#define LOAD_ARGS_5(a, b, c, d, e)    \
-    LOAD_ARGS_4 (a, b, c, d)      \
-  register long __e __asm__ ("r7") = (long)(e);
-#define ASM_ARGS_OUT_5  ASM_ARGS_OUT_4, "=r" (__e)
-#define ASM_ARGS_IN_5 ASM_ARGS_IN_4, "5" (__e)
+#define LOAD_ARGS_5(a, b, c, d, e) \
+  long _e = (long)(e);                                     \
+  LOAD_ARGS_4 (a, b, c, d)                                 \
+  register long __e __asm__ ("r7") = _e;
+#define ASM_ARGS_5 ASM_ARGS_4, "r" (__e)
 #define ASM_CLOBBERS_5  "r8", ASM_CLOBBERS_6
 
-#define LOAD_ARGS_6(a, b, c, d, e, f)   \
-    LOAD_ARGS_5 (a, b, c, d, e)     \
-  register long __f __asm__ ("r8") = (long)(f);
-#define ASM_ARGS_OUT_6  ASM_ARGS_OUT_5, "=r" (__f)
-#define ASM_ARGS_IN_6 ASM_ARGS_IN_5, "6" (__f)
+#define LOAD_ARGS_6(a, b, c, d, e, f) \
+  long _f = (long)(f);                                     \
+  LOAD_ARGS_5 (a, b, c, d, e)                              \
+  register long __f __asm__ ("r8") = _f;
+#define ASM_ARGS_6 ASM_ARGS_5, "r" (__f)
 #define ASM_CLOBBERS_6
 
 #endif
